2fba7df06adcbeb27e48a8ef13c99091b6840a23
Juan Pablo Chini <67064144+JuanChini-Dev@users.noreply.github.com>
2022-06-03 20:18:31 -0300

    Se agregaron las primeras imagenes de prueba y se agrego un boton emergente via scroll con hover

--- js/custom.js
+++ js/custom.js
@@ -85,6 +85,7 @@ $(document).ready(function () {
     } else {
       $('.main_menu').removeClass('menu_fixed animated fadeInDown')
     }
+    scrollFunctionBTT()
   })
 
   //   $(document).ready(function(){
@@ -190,3 +191,85 @@ $(document).ready(function () {
     },
   })
 })(jQuery)
+
+myButton = document.getElementById('myBtn')
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunctionBTT() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = 'block'
+  } else {
+    myButton.style.display = 'none'
+  }
+}
+
+class HoverButton {
+  constructor(el) {
+    this.el = el
+    this.hover = false
+    this.calculatePosition()
+    this.attachEventsListener()
+  }
+
+  attachEventsListener() {
+    window.addEventListener('mousemove', (e) => this.onMouseMove(e))
+    window.addEventListener('resize', (e) => this.calculatePosition(e))
+  }
+
+  calculatePosition() {
+    gsap.set(this.el, {
+      x: 0,
+      y: 0,
+      scale: 1,
+    })
+    const box = this.el.getBoundingClientRect()
+    this.x = box.left + box.width * 0.5
+    this.y = box.top + box.height * 0.5
+    this.width = box.width
+    this.height = box.height
+  }
+
+  onMouseMove(e) {
+    let hover = false
+    let hoverArea = this.hover ? 0.7 : 0.5
+    let x = e.clientX - this.x
+    let y = e.clientY - this.y
+    let distance = Math.sqrt(x * x + y * y)
+    if (distance < this.width * hoverArea) {
+      hover = true
+      if (!this.hover) {
+        this.hover = true
+      }
+      this.onHover(e.clientX, e.clientY)
+    }
+
+    if (!hover && this.hover) {
+      this.onLeave()
+      this.hover = false
+    }
+  }
+
+  onHover(x, y) {
+    gsap.to(this.el, {
+      x: (x - this.x) * 0.4,
+      y: (y - this.y) * 0.4,
+      scale: 1.15,
+      ease: 'power2.out',
+      duration: 0.4,
+    })
+    this.el.style.zIndex = 10
+  }
+  onLeave() {
+    gsap.to(this.el, {
+      x: 0,
+      y: 0,
+      scale: 1,
+      ease: 'elastic.out(1.2, 0.4)',
+      duration: 0.7,
+    })
+    this.el.style.zIndex = 1
+  }
+}
+
+const btn1 = document.getElementById('myBtn')
+new HoverButton(btn1)
