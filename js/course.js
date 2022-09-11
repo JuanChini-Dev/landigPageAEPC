2673c5375f24984f8c89b2cd14e9ee727fa433af
Juan Pablo Chini <67064144+JuanChini-Dev@users.noreply.github.com>
2022-09-11 20:48:16 -0300

    Se agrego condicional en el template string para los cursos activos y los ya iniciados

--- js/course.js
+++ js/course.js
@@ -2,30 +2,66 @@ let courses = [
   {
     title: 'Perito Clasificador de Cereales',
     url: 'https://google.com',
-    date_init: '01/03/2022',
+    date_init: '03/01/2022',
     modality: 'Presencial',
     date_string: 'Marzo 2022',
   },
   {
     title: 'Perito Clasificador de Cereales',
     url: 'https://google.com',
-    date_init: '01/05/2022',
+    date_init: '05/01/2022',
     modality: 'Distancia',
     date_string: 'Mayo 2022',
   },
   {
     title: 'Tecnicatura Superior en Calidad y Comercializacion de Granos',
     url: 'https://google.com',
-    date_init: '01/03/2022',
+    date_init: '08/01/2022',
     modality: 'Distancia',
-    date_string: 'Marzo 2022',
+    date_string: 'Agosto 2022',
+  },
+
+  {
+    title: 'Jornada de Actualizacion de Peritos Clasificadores de Cereales',
+    url: 'https://google.com',
+    date_init: '08/01/2022',
+    modality: 'Distancia',
+    date_string: 'Agosto 2022',
+  },
+  {
+    title: 'Manejo Integrado de Plagas en Granos Almacenados',
+    url: 'https://google.com',
+    date_init: '08/01/2022',
+    modality: 'Distancia',
+    date_string: 'Agosto 2022',
+  },
+  {
+    title: 'Manejo Eficiente de Granos en Poscosecha',
+    url: 'https://google.com',
+    date_init: '08/01/2022',
+    modality: 'Distancia',
+    date_string: 'Agosto 2022',
+  },
+  {
+    title: 'Manejo Eficiente de Mermas de Granos en Poscosecha',
+    url: 'https://google.com',
+    date_init: '08/01/2022',
+    modality: 'Distancia',
+    date_string: 'Agosto 2022',
+  },
+  {
+    title: 'Diplomatura Superior en Poscosecha de Granos',
+    url: 'https://google.com',
+    date_init: '08/01/2022',
+    modality: 'Distancia',
+    date_string: 'Agosto 2022',
   },
   {
     title: 'Perito Clasificador de Cereales',
     url: 'https://google.com',
-    date_init: '01/08/2022',
+    date_init: '10/12/2022',
     modality: 'Distancia',
-    date_string: 'Agosto 2022',
+    date_string: 'Septiembre 2022',
   },
 ]
 
@@ -37,6 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let course = courses.sort((a, b) => {
     return new Date(b.date_init) - new Date(a.date_init)
   })
+  console.log(course)
 
   let courseSplit = course.slice(0, 3)
 
@@ -46,8 +83,12 @@ document.addEventListener('DOMContentLoaded', function () {
         <div class="col-sm-6 col-lg-4 mb-1">
           <div class="single_special_cource">
             <img src="img/special_cource_3.png" class="special_img" alt="" />
-            <div class="special_cource_text">
-            <h4>Inicio: ${course[c].date_string}</h4>
+            <div class="special_cource_text">            
+            ${
+              new Date(course[c].date_init) > new Date()
+                ? `<h4>Inicio: ${course[c].date_string}</h4>`
+                : `<h4><s>Inicio: ${course[c].date_string}</s></h4>`
+            }
               <a href=${course[c].url} class="btn_4">${course[c].modality}</a>
               <a href=${course[c].url}>
                 <h3>${course[c].title}</h3>
@@ -57,23 +98,28 @@ document.addEventListener('DOMContentLoaded', function () {
           </div>
         </div>`
     }
+
     divIndexCourse.outerHTML = html
   } else {
     for (const c in course) {
       html += `
-        <div class="col-sm-6 col-lg-4 mb-1">
-          <div class="single_special_cource">
-            <img src="img/special_cource_3.png" class="special_img" alt="" />
-            <div class="special_cource_text">
-            <h4>Inicio: ${course[c].date_string}</h4>
-              <a href=${course[c].url} class="btn_4">${course[c].modality}</a>
-              <a href=${course[c].url}>
-                <h3>${course[c].title}</h3>
-              </a>
-            <a href="${course[c].url}" class="btn_2">Ver mas</a>
-           </div>
-          </div>
-        </div>`
+      <div class="col-sm-6 col-lg-4 mb-1">
+      <div class="single_special_cource">
+        <img src="img/special_cource_3.png" class="special_img" alt="" />
+        <div class="special_cource_text">
+        ${
+          new Date(course[c].date_init) > new Date()
+            ? `<h4>Inicio: ${course[c].date_string}</h4>`
+            : `<h4><s>Inicio: ${course[c].date_string}</s></h4>`
+        }
+          <a href=${course[c].url} class="btn_4">${course[c].modality}</a>
+          <a href=${course[c].url}>
+            <h3>${course[c].title}</h3>
+          </a>
+        <a href="${course[c].url}" class="btn_2">Ver mas</a>
+       </div>
+      </div>
+    </div>`
     }
     divCourse.outerHTML = html
   }
