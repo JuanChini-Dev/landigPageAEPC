14e737d4f32b41aac7c724eb38df8956c4b52081
Juan Pablo Chini <67064144+JuanChini-Dev@users.noreply.github.com>
2022-09-12 15:51:01 -0300

    Se agrego paginacion en el array de cursos

--- js/course.js
+++ js/course.js
@@ -65,31 +65,34 @@ let courses = [
   },
 ]
 
-document.addEventListener('DOMContentLoaded', function () {
+let firstPagination = 0
+let secondPagination = 6
+
+function loadCourses(firstPagination, secondPagination) {
   let urlHost = window.location.pathname
-  let divIndexCourse = document.getElementById('indexCourses')
-  let divCourse = document.getElementById('courses')
   let html = ''
+
   let course = courses.sort((a, b) => {
     return new Date(b.date_init) - new Date(a.date_init)
   })
-  console.log(course)
-
-  let courseSplit = course.slice(0, 3)
 
   if (!urlHost.includes('cource')) {
+    let courseSplit = course.slice(0, 3)
+    console.log(courseSplit)
+    let divIndexCourse = document.getElementById('indexCourses')
     for (const c in courseSplit) {
       html += `
         <div class="col-sm-6 col-lg-4 mb-1">
           <div class="single_special_cource">
             <img src="img/special_cource_3.png" class="special_img" alt="" />
-            <div class="special_cource_text">            
+            <div class="special_cource_text">  
             ${
               new Date(course[c].date_init) > new Date()
                 ? `<h4>Inicio: ${course[c].date_string}</h4>`
                 : `<h4><s>Inicio: ${course[c].date_string}</s></h4>`
             }
               <a href=${course[c].url} class="btn_4">${course[c].modality}</a>
+            
               <a href=${course[c].url}>
                 <h3>${course[c].title}</h3>
               </a>
@@ -98,29 +101,66 @@ document.addEventListener('DOMContentLoaded', function () {
           </div>
         </div>`
     }
-
     divIndexCourse.outerHTML = html
   } else {
-    for (const c in course) {
+    let divCourse = document.getElementById('courses')
+    let coursePagination = course.slice(firstPagination, secondPagination)
+    for (const c in coursePagination) {
       html += `
       <div class="col-sm-6 col-lg-4 mb-1">
       <div class="single_special_cource">
         <img src="img/special_cource_3.png" class="special_img" alt="" />
         <div class="special_cource_text">
         ${
-          new Date(course[c].date_init) > new Date()
-            ? `<h4>Inicio: ${course[c].date_string}</h4>`
-            : `<h4><s>Inicio: ${course[c].date_string}</s></h4>`
+          new Date(coursePagination[c].date_init) > new Date()
+            ? `<h4>Inicio: ${coursePagination[c].date_string}</h4>`
+            : `<h4><s>Inicio: ${coursePagination[c].date_string}</s></h4>`
         }
-          <a href=${course[c].url} class="btn_4">${course[c].modality}</a>
-          <a href=${course[c].url}>
-            <h3>${course[c].title}</h3>
+          <a href=${coursePagination[c].url} class="btn_4">${
+        coursePagination[c].modality
+      }</a>
+          <a href=${coursePagination[c].url}>
+            <h3>${coursePagination[c].title}</h3>
           </a>
-        <a href="${course[c].url}" class="btn_2">Ver mas</a>
+        <a href="${coursePagination[c].url}" class="btn_2">Ver mas</a>
        </div>
       </div>
     </div>`
     }
-    divCourse.outerHTML = html
+    divCourse.innerHTML = html
   }
+}
+
+document.addEventListener('click', function (e) {
+  if (e.target.matches('.page-item a')) {
+    e.preventDefault()
+    scrollTo(0, 0, 'smooth')
+    document.getElementById('courses').innerHTML = ''
+    if (e.target.textContent == 'Next') {
+      firstPagination = firstPagination + 6
+      secondPagination = secondPagination + 6
+      if (firstPagination > 0) {
+        document
+          .getElementById('page-item-previous')
+          .classList.remove('disabled')
+      }
+      if (secondPagination >= courses.length) {
+        document.getElementById('page-item-next').classList.add('disabled')
+      }
+      loadCourses(firstPagination, secondPagination)
+    } else {
+      firstPagination = firstPagination - 6
+      secondPagination = secondPagination - 6
+      if (firstPagination <= 0) {
+        document.getElementById('page-item-previous').classList.add('disabled')
+        document.getElementById('page-item-next').classList.remove('disabled')
+      }
+
+      loadCourses(firstPagination, secondPagination)
+    }
+  }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  loadCourses(firstPagination, secondPagination)
 })
